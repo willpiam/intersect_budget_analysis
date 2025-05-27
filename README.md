c74e2d1db808971e7401a316a76efd231aa8cdc6
willpiam <willdoyle422@gmail.com>
2025-05-27 15:36:33 -0400

    clean up a bit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Intersect Budget Analysis 2025
+
+This project analyzes and visualizes the budget requests from Cardano proposals. The analysis includes three different views of the proposal amounts to help understand the distribution of funding requests.
+
+## Generated Charts
+
+### All Proposals
+![All Proposals](charts/all_proposals.png)
+
+### Top Half - Most Expensive Proposals
+![Top Half Proposals](charts/top_half_proposals.png)
+
+### Bottom Half - Least Expensive Proposals
+![Bottom Half Proposals](charts/bottom_half_proposals.png)
+
+
+## Running the Analysis
+
+To generate these charts, run the analysis script. The charts will be generated in the `charts` directory. 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 import statistics
+import os
 
 def create_bar_plot(proposals, title, filename, figsize=(20, 12)):
     plt.figure(figsize=figsize)
@@ -41,12 +42,18 @@ def create_bar_plot(proposals, title, filename, figsize=(20, 12)):
     print(f"Bar graph has been saved as '{filename}'")
 
 def main():
+    # Create charts directory if it doesn't exist
+    charts_dir = 'charts'
+    if not os.path.exists(charts_dir):
+        os.makedirs(charts_dir)
+    
     # Read the JSON file
     with open('data.json', 'r') as file:
         proposals = json.load(file)
     
     # Calculate total number of proposals
     total_proposals = len(proposals)
+    assert total_proposals == 39, f"Total proposals should be 39, but is {total_proposals}"
     
     # Calculate total amount requested
     total_amount = sum(proposal['amount'] for proposal in proposals)
@@ -62,22 +69,16 @@ def main():
     mid_point = len(sorted_proposals) // 2
     top_half = sorted_proposals[:mid_point]
     bottom_half = sorted_proposals[mid_point:]
-
+    
     # add asserts to ensure this makes sense
     assert len(top_half) + len(bottom_half) == len(proposals)
     # all elements of proposals is either in top or bottom (not using sets) using lambda
     assert all(lambda x: x in top_half or x in bottom_half for x in proposals)
     
-
-
-
-
-
-    
     # Create three separate plots
-    create_bar_plot(sorted_proposals, 'All Proposals by Amount Requested', 'all_proposals.png')
-    create_bar_plot(top_half, 'Top Half - Most Expensive Proposals', 'top_half_proposals.png')
-    create_bar_plot(bottom_half, 'Bottom Half - Least Expensive Proposals', 'bottom_half_proposals.png')
+    create_bar_plot(sorted_proposals, 'All Proposals by Amount Requested', os.path.join(charts_dir, 'all_proposals.png'))
+    create_bar_plot(top_half, 'Top Half - Most Expensive Proposals', os.path.join(charts_dir, 'top_half_proposals.png'))
+    create_bar_plot(bottom_half, 'Bottom Half - Least Expensive Proposals', os.path.join(charts_dir, 'bottom_half_proposals.png'))
 
 if __name__ == "__main__":
     main()
